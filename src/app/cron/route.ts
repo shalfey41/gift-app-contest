bfa2f55fb62959a18c63191e768366f3ca593ea0
shalfey41 <d.e.bezugly@gmail.com>
2024-11-10 03:07:37 +0400

    Changed cron

--- src/app/cron/route.ts
+++ src/app/cron/route.ts
@@ -4,8 +4,6 @@ import { deleteExpiredInvoices } from '@/modules/activeInvoice/service';
 export const dynamic = 'force-dynamic';
 
 export function GET(request: Request) {
-  console.log('CRON STARTED');
-
   if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
