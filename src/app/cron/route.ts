4820eeeffd0e4691812a1c5b5608f1a5c8c16d49
shalfey41 <d.e.bezugly@gmail.com>
2024-11-10 03:11:29 +0400

    Fixed cron

--- src/app/cron/route.ts
+++ src/app/cron/route.ts
@@ -3,6 +3,7 @@ import { deleteExpiredInvoices } from '@/modules/activeInvoice/service';
 
 export const dynamic = 'force-dynamic';
 
+// Runs cron every day at 10 AM just for the demo purposes (in real life, I would run it every 15 minutes)
 export function GET(request: Request) {
   if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
